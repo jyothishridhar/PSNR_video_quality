84a1c348f2e388274cf6ceeb582aacc51fcc4e4f
Jyothi S <jyothi.s@genisys-group.com>
2023-12-01 15:47:45 +0530

    Initial commit

---
+++ psnr.py
@@ -0,0 +1,89 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Rest of the code is the same
+def calculate_psnr(frame1, frame2):
+    mse = np.mean((frame1 - frame2) ** 2)
+    if mse == 0:
+        return float('inf')
+    max_pixel = 255.0
+    psnr = 10 * np.log10((max_pixel ** 2) / mse)
+    return psnr
+
+def calculate_psnr_for_each_frame(distorted_video_path, good_video_path):
+    # Open the videos
+    distorted_video = cv2.VideoCapture(distorted_video_path)
+    good_video = cv2.VideoCapture(good_video_path)
+
+    psnr_values = []
+    distorted_frame_numbers = []
+    frame_timestamps = []  # To store the timestamp of each frame
+
+    while True:
+        # Read frames from both videos
+        ret1, distorted_frame = distorted_video.read()
+        ret2, good_frame = good_video.read()
+
+        # If frames are not retrieved, the videos have ended
+        if not ret1 or not ret2:
+            break
+
+        # Convert frames to grayscale
+        distorted_frame_gray = cv2.cvtColor(distorted_frame, cv2.COLOR_BGR2GRAY)
+        good_frame_gray = cv2.cvtColor(good_frame, cv2.COLOR_BGR2GRAY)
+
+        # Calculate PSNR for the current frames
+        psnr = calculate_psnr(distorted_frame_gray, good_frame_gray)
+
+        # Append the PSNR value to the list
+        psnr_values.append(psnr)
+
+        # Check if distortion happens (e.g., PSNR is below a threshold)
+        if psnr < 25.0:
+            distorted_frame_numbers.append(len(psnr_values))
+
+        # Get the timestamp of the current frame and append to the list
+        current_frame_time = distorted_video.get(cv2.CAP_PROP_POS_MSEC)
+        frame_timestamps.append(current_frame_time)
+
+    # Release the videos
+    distorted_video.release()
+    good_video.release()
+
+    return psnr_values, distorted_frame_numbers, frame_timestamps
+
+# Rest of the code remains the same
+
+# Example usage
+distorted_video_path = r"C:/OTT_PROJECT/SSIM and PSNR/output_video_distorted_middle.avi"
+good_video_path = r"C:/OTT_PROJECT/SSIM and PSNR/oxford_referance.mp4"
+
+# Calculate PSNR values for each frame in the distorted video
+psnr_values, distorted_frame_numbers, frame_timestamps = calculate_psnr_for_each_frame(distorted_video_path, good_video_path)
+
+# Create a list of frame numbers for x-axis
+frame_numbers = list(range(1, len(psnr_values) + 1))
+
+# Plot the PSNR values in a line graph
+plt.plot(frame_numbers, psnr_values, marker='o', linestyle='-')
+plt.xlabel('Frame Number')
+plt.ylabel('PSNR Value')
+plt.title('PSNR Values for Distorted Video')
+plt.grid(True)
+plt.show()
+
+# Print PSNR values and frame timestamps for each frame
+data = {
+    'Frame Number': frame_numbers,
+    'PSNR Value': psnr_values,
+    'Timestamp (ms)': frame_timestamps
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+# Save PSNR values, frame numbers, and timestamps to an Excel file
+output_excel_path = r"C:\OTT_PROJECT\SSIM and PSNR\PSNR\psnr_values_and_timestamps.xlsx"
+df.to_excel(output_excel_path, index=False)
