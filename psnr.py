2131eb5a52429fd4a24909b5406b2b2e83ddf724
Jyothi S <jyothi.s@genisys-group.com>
2023-12-04 10:36:07 +0530

    Update requirements.txt

--- psnr.py
+++ psnr.py
@@ -100,3 +100,13 @@ st.dataframe(df)
 # Save PSNR values, frame numbers, and timestamps to an Excel file
 excel_buffer = df.to_excel(index=False)
 st.markdown(get_excel_link(excel_buffer, "Download PSNR Report"), unsafe_allow_html=True)
+
+# Create download links for the videos
+st.markdown(
+    f"Download [Reference Video](data:video/mp4;base64,{base64.b64encode(open(good_video_path, 'rb').read()).decode()})",
+    unsafe_allow_html=True
+)
+st.markdown(
+    f"Download [Distorted Video](data:video/mp4;base64,{base64.b64encode(open(distorted_video_path, 'rb').read()).decode()})",
+    unsafe_allow_html=True
+)
