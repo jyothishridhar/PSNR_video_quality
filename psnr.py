fca470fba75da1d5af51aa1af7f8ffc190742be6
Jyothi S <jyothi.s@genisys-group.com>
2023-12-04 10:11:19 +0530

    Update requirements.txt

--- psnr.py
+++ psnr.py
@@ -4,8 +4,6 @@ import numpy as np
 import pandas as pd
 import tempfile
 import os
-import matplotlib
-matplotlib.use("Agg")  # Use the Agg backend which doesn't require a display
 import matplotlib.pyplot as plt
 
 
